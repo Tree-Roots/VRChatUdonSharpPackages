e70556ade7b39bce0c8f0c7f8e4c7d6136a779bd
Tree Roots <109141447+Tree-Roots@users.noreply.github.com>
2022-08-14 16:43:18 +0800

    1.1.0

---
+++ Packages/com.vrchat.udonsharp/Editor/UdonSharpProgramAssetPostprocessor.cs
@@ -0,0 +1,28 @@
+﻿
+using UdonSharp;
+using UnityEditor;
+
+namespace UdonSharpEditor
+{
+    internal class UdonSharpProgramAssetPostprocessor : AssetPostprocessor
+    {
+        private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+        {
+            foreach (string importedAssetPath in importedAssets)
+            {
+                UdonSharpProgramAsset importedAsset = AssetDatabase.LoadAssetAtPath<UdonSharpProgramAsset>(importedAssetPath);
+
+                if (importedAsset && (importedAsset.CompiledVersion < UdonSharpProgramVersion.CurrentVersion || importedAsset.ScriptVersion < UdonSharpProgramVersion.CurrentVersion))
+                {
+                    UdonSharpUpgrader.QueueUpgrade(importedAsset);
+                    UdonSharpEditorCache.Instance.QueueUpgradePass();
+                }
+
+                if (importedAsset)
+                {
+                    UdonSharpProgramAsset.ClearProgramAssetCache();
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# VRChat UdonSharp Packages
+
+#### 介绍
+
+同步 VRChat Creator Companion 里的包。
+
+#### 当前版本
+
+| 名称      | 版本  | 克隆 URL                                                                                |
+| --------- | ----- | --------------------------------------------------------------------------------------- |
+| UdonSharp | 1.1.0 | https://github.com/Tree-Roots/VRChatUdonSharpPackages.git?path=/Packages/com.vrchat.udonsharp#1.1.0 |
+
+#### 安装 UdonSharp
+
+1. 打开 Package Manager
+2. 点击“+”号，再点击“Add package from git URL...”
+3. 输入“https://github.com/Tree-Roots/VRChatUdonSharpPackages.git?path=/Packages/com.vrchat.udonsharp#1.1.0”并回车。
+
+#### 特点
+
+1. 无需安装 VRChat Creator Companion 即可获取最新的 VRChat SDK
